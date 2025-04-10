78a71c15b4300dba2e75d6a1293513c0e6a0633a
Pedro Altoé Costa <pedroaltoecosta@gmail.com>
2025-04-10 13:09:38 +0100

    Initial Commit

---
+++ ReceiptTracker/ReceiptTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReceiptTrackerApp.swift
+//  ReceiptTracker
+//
+//  Created by Pedro Altoe Costa on 10/4/2025.
+//
+
+import SwiftUI
+
+@main
+struct ReceiptTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ReceiptTrackerUITests/ReceiptTrackerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  ReceiptTrackerUITestsLaunchTests.swift
+//  ReceiptTrackerUITests
+//
+//  Created by Pedro Altoe Costa on 10/4/2025.
+//
+
+import XCTest
+
+final class ReceiptTrackerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
